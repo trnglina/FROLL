9829a45221bb51c87b41ce985535d3fa6741fd57
Elsseriah Decieux <pokeplun@gmail.com>
2017-07-19 18:29:29 -0600

    qol

--- FROLL.F90
+++ FROLL.F90
@@ -192,21 +192,26 @@ contains
     end subroutine init_random_seed
 end module froll
 
-program froll_test
+program exec_froll
     use froll
     implicit none
 
-    character(len=100) :: input
+    character(len=100) :: input, args
     integer :: i
     type(roll_result) :: exec_roll_result
 
-    write(*,'("Enter dice notation: ")')
-    read(*,'(A)') input
+    do while(.true.)
 
-    exec_roll_result = roll(input)
-    do i = 1, size(exec_roll_result%rolls)
-        write(*,*) "roll: ", exec_roll_result%rolls(i)
+        call get_command_argument(1, args)
+
+        write(*,'("Enter dice notation: ")')
+        read(*,'(A)') input
+
+        exec_roll_result = roll(input)
+        do i = 1, size(exec_roll_result%rolls)
+            write(*,*) "roll: ", exec_roll_result%rolls(i)
+        end do
+        write(*,*) "mod : ", exec_roll_result%modifier
+        write(*,*) "tot : ", exec_roll_result%total
     end do
-    write(*,*) "mod : ", exec_roll_result%modifier
-    write(*,*) "tot : ", exec_roll_result%total
-end program froll_test
+end program exec_froll
